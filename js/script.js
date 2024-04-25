f236c678c1ccb84bca6939b716147fa3dae2b658
Moscaliuc <moscaliucalexandru4@gmail.com>
2024-04-25 16:24:44 +0300

    Add rgb color on mouseenter on square

--- js/script.js
+++ js/script.js
@@ -7,4 +7,18 @@ button.addEventListener('click', ()=>{
     const patrat = document.createElement('div')
     patrat.classList.add('patrat')
     body.append(patrat)
+
+    patrat.addEventListener('mouseenter', (e)=>{
+        let r = Math.floor(Math.random()*255)
+        let g = Math.floor(Math.random()*255)
+        let b = Math.floor(Math.random()*255)
+        patrat.style.backgroundColor = `rgb(${r},${g},${b})`
+    })
+    patrat.addEventListener('mouseleave', (e)=>{
+        let r = Math.floor(Math.random()*255)
+        let g = Math.floor(Math.random()*255)
+        let b = Math.floor(Math.random()*255)
+        patrat.style.backgroundColor = `rgb(${r},${g},${b})`
+    })
 })
+
